34e17313af7ebeb0ed23f18d653e23abdf868936
ryarasi <ragav.code@gmail.com>
2021-04-01 15:27:22 +0530

    tab params addition pending

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+const tabIndexList = {
+  admin: 1,
+  announcement: 2,
+  assignment: 3,
+  course: 4,
+  group: 5,
+  report: 6,
+};
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  activeTab = 0;
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.activeTab = tabIndexList[params['tab']]
+        ? tabIndexList[params['tab']]
+        : this.activeTab;
+    });
   }
-
+  navigate($event) {
+    const tabName = $event['tab']['textLabel'];
+    console.log('navigate to ', $event, 'tabName', tabName);
+    this.router.navigate([this.route], {
+      relativeTo: this.route,
+      queryParams: { tab: ['tabName'] },
+      queryParamsHandling: 'merge',
+      // preserve the existing query params in the route
+      skipLocationChange: true,
+      // do not trigger navigation
+    });
+  }
 }
