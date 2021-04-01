2e169e9347a5fe426956c52c6f0a597192430632
ryarasi <ragav.code@gmail.com>
2021-04-01 14:47:51 +0530

    Added the tabs in dashboard, customized styling of the tab using mat-color change in style.css

---
+++ src/app/pages/dashboard/tabs/announcement-dashboard/announcement-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-dashboard',
+  templateUrl: './announcement-dashboard.component.html',
+  styleUrls: ['./announcement-dashboard.component.scss']
+})
+export class AnnouncementDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
