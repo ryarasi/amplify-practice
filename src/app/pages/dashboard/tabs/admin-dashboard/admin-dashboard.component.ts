b1f97add8edbaa7733fa5c37da20d6827f4bcd20
ryarasi <ragav.code@gmail.com>
2021-04-04 19:05:22 +0530

    Added the left menu on the admin dashboard

--- src/app/pages/dashboard/tabs/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/dashboard/tabs/admin-dashboard/admin-dashboard.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class AdminDashboardComponent implements OnInit {
   opened: boolean;
   iconName = 'arrow_forward';
+  entities: string[] = ['Schools', 'Classes', 'Groups'];
+
   constructor() {}
 
   ngOnInit(): void {}
